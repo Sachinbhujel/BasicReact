3fdc1738fc9ab5006f042112f1ba46eab709da0e
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2024-06-04 17:35:26 +0000

    Basic React

---
+++ UseReact_hook/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import { useRef } from 'react'
+
+function App() {
+  let ref = useRef();
+  let displayRef = useRef();
+
+  function handleRefChange(){
+    displayRef.current.textContent  = ref.current.value;
+    ref.current.focus();
+  }
+
+  return (
+    <div className='main'>
+      <h3 ref={displayRef}>Display Here</h3>
+      <input type="text" ref={ref} placeholder='Enter Here'/>
+      <button onClick={handleRefChange}>Click</button>
+    </div>
+  )
+}
+
+export default App
