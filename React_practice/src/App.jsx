fc4c270c70d3ec3ce34c46d5644d1b4954879563
Sachin Bhujel <90704616+Sachinbhujel@users.noreply.github.com>
2025-01-31 12:06:58 +0000

    commit

---
+++ React_practice/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+class Car extends React.Component{
+  render(){
+    return (
+      <>
+        <h1>Hiiii</h1>
+        <h1>Hiiii</h1>
+      </>
+    )
+  }
+}
+
+export default Car
